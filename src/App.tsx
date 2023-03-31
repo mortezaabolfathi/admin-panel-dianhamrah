686044b6f233ce5f85c27f112a4292367e14f8f8
Morteza <morteza.abolfathi73@gmail.com>
2023-03-31 11:43:01 +0430

    feat: global style, none arrow input when type="number"

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Layout from "./Layout";
+import SingIn from "./pages/SingIn";
 
 function App() {
   return (
    <Layout/>
+    // <SingIn/>
   )}
 
 export default App;
