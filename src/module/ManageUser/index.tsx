fed069618bd397c60c3d9f3786e7b04dcb8fa57f
Morteza <morteza.abolfathi73@gmail.com>
2023-04-04 23:30:08 +0430

    feat: Destructuring  data and show in table

--- src/module/ManageUser/index.tsx
+++ src/module/ManageUser/index.tsx
@@ -15,71 +15,7 @@ import { TableColumnBase } from "react-data-table-component/dist/src/DataTable/t
 
 function ManageUser() {
 
-  const {data:users} = useGetUsersQuery("");
-  console.log(users)
-
-  // const columns = [
-  //   { name: "عکس", selector: (rwo: any) => rwo.img, grow: 1, center: true },
-  //   {
-  //     name: "نام کاربران",
-  //     selector: (row: any) => row.title,
-  //     grow: 2,
-  //     center: true,
-  //   },
-  //   {
-  //     name: "کدملی",
-  //     selector: (row: any) => toFarsiNumber(row.personalId),
-  //     grow: 2,
-  //     center: true,
-  //   },
-  //   {
-  //     name: "شماره تماس",
-  //     selector: (row: any) => toFarsiNumber(row.phoneNumber),
-  //     grow: 2,
-  //     center: true,
-  //   },
-  
-  //   {
-  //     name: "مدیریت",
-  //     selector: (row: any) => (
-  //       <div className="flex gap-4">
-  //         <Tooltip content="گواهی نامه">
-  //           <div
-  //             className="cursor-pointer text-2xl  hover:text-blue-500"
-  //             onClick={(e) => {
-  //               console.log("certificatied");
-  //             }}
-  //           >
-  //             {row.certificationStory}
-  //           </div>
-  //         </Tooltip>
-  //         <Tooltip content="حذف کاربر">
-  //           <div
-  //             className="cursor-pointer text-2xl  hover:text-blue-500"
-  //             onClick={() => {
-  //               ModalDelete();
-  //             }}
-  //           >
-  //             {row.deleteUSer}
-  //           </div>
-  //         </Tooltip>
-  //         <Tooltip content="ویرایش">
-  //           <div
-  //             className="cursor-pointer text-2xl  hover:text-blue-500"
-  //             onClick={() => {
-  //               ModalEdit();
-  //             }}
-  //           >
-  //             {row.editUser}
-  //           </div>
-  //         </Tooltip>
-  //       </div>
-  //     ),
-  //     grow: 1,
-  //     center: true,
-  //   },
-  // ];
-  
+  const {data:users} = useGetUsersQuery("" );
   
   const columns:any = [
     {name:"نام ", selector: (rwo: any) =><span className="text-[16px]">{rwo.firstName}</span> , center: true },
